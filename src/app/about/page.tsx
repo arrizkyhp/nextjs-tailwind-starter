330116c2638320d10b61481d8548414649078e9a
arrizkyhp <arrizkyhasya@gmail.com>
2024-05-03 14:56:14 +0700

    feat: add shadcn ui

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Button} from "@@/components/ui/button";
+
+function Page() {
+    return (
+        <div><Button variant="destructive">Click me</Button></div>
+    );
+}
+
+export default Page;
